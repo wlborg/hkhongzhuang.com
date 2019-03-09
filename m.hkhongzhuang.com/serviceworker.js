fa65f3ac6e19bd22a5647768695ea1522e1bc7ca
August1Rush <271945920@qq.com>
2019-03-09 11:30:15 +0800

    首页样式
    首页样式

--- m.hkhongzhuang.com/serviceworker.js
+++ m.hkhongzhuang.com/serviceworker.js
@@ -1,5 +1,5 @@
 'use strict';
-const version = 'v20190309';
+const version = 'v201903091130';
 const __DEVELOPMENT__ = false;
 const __DEBUG__ = false;
 const offlineResources = ['/', '/offline.html', '/offline.svg'];
