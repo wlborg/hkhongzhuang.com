245d41d916ae95ceca572d1672c7236b76f12f08
August1Rush <271945920@qq.com>
2019-03-09 11:36:02 +0800

    首页图更换
    首页图更换

--- m.hkhongzhuang.com/serviceworker.js
+++ m.hkhongzhuang.com/serviceworker.js
@@ -1,5 +1,5 @@
 'use strict';
-const version = 'v201903091130';
+const version = 'v201903091135';
 const __DEVELOPMENT__ = false;
 const __DEBUG__ = false;
 const offlineResources = ['/', '/offline.html', '/offline.svg'];
