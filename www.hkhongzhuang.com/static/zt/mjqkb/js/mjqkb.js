024455138cd7b569a848681a23c5f2979a8243ce
workWebGit <779723512@qq.com>
2019-09-09 10:19:08 +0800

    专题文件
    专题文件

---
+++ www.hkhongzhuang.com/static/zt/mjqkb/js/mjqkb.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2019/7/4.
+ */
+$(function(){
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 4,
+        loop:true,
+        spaceBetween: 23,
+        navigation: {
+            nextEl: '.part10 .swiper-button-next',
+            prevEl: '.part10 .swiper-button-prev',
+        },
+    });
+
+});
